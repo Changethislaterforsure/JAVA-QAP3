af6ab153d6f3066b662eeba9b2fa15576ab48734
changethislaterforsure <benjmain.legge@keyin.comm>
2025-07-11 21:30:59 -0230

    Created the other triangle, too tired to spell the name right

---
+++ Problem 3/EquilateralTriangle.java
@@ -0,0 +1,7 @@
+
+public class EquilateralTriangle extends Triangle {
+    public EquilateralTriangle(double sideLength) {
+        super(sideLength, sideLength, sideLength);
+        this.name = "Equilateral Triangle";
+    }
+}
